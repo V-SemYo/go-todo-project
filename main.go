da6de28f75089e0ada865f6dec71a7f7b9215884
Иван Семенов <ran46@yandex.ru>
2026-04-14 10:13:14 +0300

    step2: database init, always create table

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"log"
 	"os"
 
+	"go-todo-project/pkg/db"
 	"go-todo-project/pkg/server"
 )
 
 func main() {
+	dbFile := "scheduler.db"
+	if envFile := os.Getenv("TODO_DBFILE"); envFile != "" {
+		dbFile = envFile
+	}
+
+	if err := db.InitDB(dbFile); err != nil {
+		log.Fatal("database initalisation error ", err)
+	}
+	defer db.DB.Close()
+
 	port := os.Getenv("TODO_PORT")
 	if port == "" {
 		port = "7540"
---
+++ pkg/db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+var DB *sql.DB
+
+const schema = `
+CREATE TABLE IF NOT EXISTS scheduler (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+date CHAR(8) NOT NULL DEFAULT "",
+title VARCHAR(255) NOT NULL DEFAULT "",
+comment TEXT NOT NULL DEFAULT "",
+repeat VARCHAR(128) NOT NULL DEFAULT ""
+);
+
+CREATE INDEX IF NOT EXISTS index_date ON scheduler(date);
+`
+
+func InitDB(dbFile string) error {
+	var err error
+	DB, err := sql.Open("sqlite", dbFile)
+	if err != nil {
+		return fmt.Errorf("database %s opening error: %w", dbFile, err)
+	}
+	if err = DB.Ping(); err != nil {
+		return fmt.Errorf("database ping error: %w", err)
+	}
+
+	_, err = DB.Exec(schema)
+	if err != nil {
+		return fmt.Errorf("table creating error: %w", err)
+	}
+	return nil
+}
